a5e3b0b6090adee4e406c16ca894af13d831ec56
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2023-06-20 12:12:14 -0300

    Adiciona a opção de executar os exercícios em App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import aula1.DuplicadosNao;
+import aula1.MultiplicaAi;
+import aula1.PiramideDeNumeros;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Bem vindo!");
+        System.out.println("Bem vindo! Para testar os exercícios basta retirar o comentário.");
+        System.out.println("Se quiser executar um exercício sem passar atributos use o método main. EX: DuplicadosNao.main(args);");
+
+        //MultiplicaAi.showTable(9);
+        //PiramideDeNumeros.showPyramid(1, 8);
+        //DuplicadosNao.showDuplicates(list1, list2); //crie duas listas de String
+        //DuplicadosNao.main(args);
     }
 }
